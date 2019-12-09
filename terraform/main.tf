5cfa061a8a19f33fc717f0dc3a365b65ff5815eb
stakot <madstas@mail.ru>
2019-12-09 21:23:48 +0200

    ====ДЗ 4====

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "centos" {
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.centos.id}"
   instance_type = "t2.micro"
-  security_groups = ["${aws_security_group.hillel.name}"]
+  security_groups = ["${aws_security_group.ssh.name}","${aws_security_group.jenkins.name}"]
   key_name   = "hillel2"
   
   root_block_device {
@@ -37,16 +37,25 @@ resource "aws_instance" "web" {
       "sudo yum -y install  java-1.8.0-openjdk.x86_64",
       "sudo yum -y install jenkins",
       "sudo service jenkins start",     
-      "sudo echo '${file("/home/sko/edu/terraform/templates_nginx_config.tmpl")}' >> ~/jenkins.conf",
-      "sudo cp ~/jenkins.conf /etc/nginx/conf.d/jenkins.conf",
+#     "sudo echo '${file("/home/sko/edu/terraform/template_nginx_config.tmpl")}' >> ~/jenkins.conf",
+#     "sudo cp ~/jenkins.conf /etc/nginx/conf.d/jenkins.conf",
       "sudo rm /etc/nginx/conf.d/default.conf"
     ]
+    }
+    provisioner "file" {
+    content = "${templatefile("/home/sko/edu/terraform/template_nginx_config.tmpl", {server_name = "${self.public_ip}"})}"
+    destination = "/tmp/jenkins.conf"
+  }
+    provisioner "remote-exec" {
+      inline = [
+        "sudo cp /tmp/jenkins.conf /etc/nginx/jenkins.conf",
+        "sudo service jenkins start"
+      ]
+    }
   connection {
     type     = "ssh"
     user     = "centos"
     private_key = "${file("/home/sko/Documents/hillel2.pem")}"
     host = "${element(aws_instance.web.*.public_ip, 0)}"
   }
-}
-    
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group" "hillel" {
-  name = "web-node"
+resource "aws_security_group" "ssh" {
+  name = "ssh"
   description = "Hillel Security Group"
   ingress {
     from_port = 22
@@ -11,6 +11,22 @@ resource "aws_security_group" "hillel" {
     from_port = 0
     to_port = 0
     protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "jenkins" {
+  name = "jenkins"
+  description = "Hillel Security Group"
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }    
+  egress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
